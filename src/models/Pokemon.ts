8b20f14ad6d9c7c2c9831cc60399f51c4ca7daf6
Ioann Jeanlao Gorvier <aquonelocal@gmail.com>
2024-03-19 22:51:16 +0500

    Created project directory and structured files

---
+++ src/models/Pokemon.ts
@@ -0,0 +1,23 @@
+export interface PokemonPage {
+    result: {  name: string }[],
+    next: string | null,
+    previous: string | null,
+}
+
+export interface Pokemon {
+    name:string,
+    types: {
+        type: {
+            name: string,
+        }
+    }[],
+    weight: number,
+    height: number,
+    sprites: {
+        other: {
+            "official-artwork" : {
+                front_default: string,
+            }
+        }
+    }   
+}
