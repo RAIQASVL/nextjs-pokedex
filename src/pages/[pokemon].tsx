78765715e8102aac534504df57332716271b373b
Ioann Jeanlao Gorvier <aquonelocal@gmail.com>
2024-03-20 16:52:56 +0500

    Updated project, design and structured files

--- src/pages/[pokemon].tsx
+++ src/pages/[pokemon].tsx
@@ -1,16 +1,15 @@
 import { useRouter } from "next/router";
-import useSWR from "swr";
-import * as PokemonApi from "@/network/pokemon-api";
 import Head from "next/head";
 import Link from "next/link";
 import { Spinner } from "react-bootstrap";
 import Image from "next/image";
+import usePokemon from "@/hooks/usePokemon";
 
 export default function PokemonDetailsPage() {
     const router = useRouter();
     const pokemonName = router.query.pokemon?.toString() || "";
     
-    const { data: pokemon, isLoading: pokemonLoading } = useSWR(pokemonName, PokemonApi.getPokemon);
+    const {pokemon, pokemonLoading} = usePokemon(pokemonName);
 
     return(
         <>
@@ -21,6 +20,7 @@ export default function PokemonDetailsPage() {
             <div className="d-flex flex-column align-items-center">
                 <p><Link href="/" className="link-light">← PokeDex</Link></p>
                 {pokemonLoading && <Spinner animation="grow" />}
+                {pokemon === null && <p>Pokemon not found</p>}
                 {pokemon && 
                     <>
                         <h1 className="text-center text-capitalize">{pokemon.name}</h1>
